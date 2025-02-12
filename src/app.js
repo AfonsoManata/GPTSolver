ce19780d81b6d8632e26a47fd7595fbcc763e363
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-02-12 11:33:38 +0000

    Packages

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-// This is the code that makes the app work 
-
+//This is the code that makes the app work 
 require('dotenv').config();
 const API_KEY = process.env.API_KEY; // You can put your api key here 
 
