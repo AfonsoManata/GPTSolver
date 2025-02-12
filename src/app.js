9d3883b15afa55c5ab096d410f2129eb9ccc04be
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-02-12 11:39:34 +0000

    Principal Fuctions

--- src/app.js
+++ src/app.js
@@ -9,3 +9,52 @@ const { OpenAI } = require('openai');
 const API_KEY = process.env.API_KEY; // You can put your api key here 
 
 
+function removeallfiles(directory){
+  
+  const files = fs.readdirSync(directory); // Reading the directory
+  
+  // Deleting each screenshot
+  for (const file of files){  
+    const filepath = path.join(directory, file); 
+    fs.unlinkSync(filepath);
+  }
+  
+}
+
+function get_last_screenshot(directory){
+  
+  const files = fs.readdirSync(directory);
+
+  files.reverse();
+  
+  const last_path = path.join(directory, files[0]);
+
+  return last_path; 
+}
+
+const image = get_last_screenshot(dir);
+
+// Using tesseract js to extract the text from the image 
+const getTextFromImage = async (imagePath) => {
+  const worker = await createWorker();
+
+  const text = await worker.recognize(imagePath);
+  await worker.terminate();
+  return text;
+};
+
+(async () => {
+  try {
+    const { data: { text } } = await getTextFromImage(image);
+    
+   } catch (error) {
+    console.error('Error:', error);
+  }
+})();
+
+
+
+
+//if files = [] continue;
+// make the folder of screenshots empty  
+//removeallfiles(dir);
