72fdb9c091bace8f1ce96cc1bb7647292a7dc01c
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-02-12 11:37:36 +0000

    Imports

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
-//This is the code that makes the app work 
+// Imports needed 
 require('dotenv').config();
+const { createWorker } = require('tesseract.js');
+const os = require('os');
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(process.env.HOME, "Desktop", "Screenshots"); // Here change to your Screenshot folder
+const { OpenAI } = require('openai');
 const API_KEY = process.env.API_KEY; // You can put your api key here 
 
 
