99a9bd9d8588e6624a855634f8f5427634ac4f6f
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-02-12 11:31:00 +0000

    Putting Api Key

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-// This is the code that makes the app work
+// This is the code that makes the app work 
+
+require('dotenv').config();
+const API_KEY = process.env.API_KEY; // You can put your api key here 
+
+
