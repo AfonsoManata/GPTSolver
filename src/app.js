d76075bdc8bfcd94cf844b3370e7c16cd326641f
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-04-22 17:18:59 +0100

    Comments

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const os = require('os');
 const fs = require("fs");
 const path = require("path");
 const clipboardy = require('clipboardy');
-const dir = path.join(process.env.HOME, "Desktop", "Screenshots"); // Here change to your Screenshot folder
+const dir = path.join(process.env.HOME, "Desktop", "Screenshots");
 const { OpenAI } = require('openai');
 const API_KEY = process.env.API_KEY; // You can put your api key here 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
